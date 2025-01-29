5cd6b4083f02c6c230c18fefda66d20927492921
Janiel Marques <janielmarques@icloud.com>
2025-01-28 23:54:53 -0300

    Pilha de chamadas de métodos(stack trace)

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,10 +4,24 @@ import java.util.Scanner;
 public class Program {
 
     public static void main(String[] args) {
+
+        method1();
         
 
-        Scanner sc = new Scanner(System.in);
+        System.out.println("End of program!");
 
+    }
+
+    public static void method1() {
+        System.out.println("***START MEETHOD1***");
+        method2();
+        System.out.println("***END METHOD1***");
+    }
+
+
+   public static void method2() {
+    System.out.println("***METHOD 2 START ***");
+    Scanner sc = new Scanner(System.in);
 
     try {
         String[] vect = sc.nextLine().split(" ");
@@ -16,19 +30,15 @@ public class Program {
     }
     catch (ArrayIndexOutOfBoundsException e) {
         System.out.println("Invalid position");
+        e.printStackTrace();
+        sc.next();
     }
     catch (InputMismatchException e) {
         System.out.println("input error!");
     }
-    System.out.println("End of program!");
+    sc.close();
+        System.out.println("***METHOD2 END***");
 
-
-
-
-
-
-
-        sc.close();
     }
     
 }
